a7e9e31ee52aa7d68a86a635b59a6d29615f438c
Dimi Tsonev <dconev@gmail.com>
2024-01-22 22:28:53 +0000

    manager,intern,enjineer questions  and validation for email,phone number and id

--- starter/index.js
+++ starter/index.js
@@ -10,6 +10,103 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamArray = [];
+
+const validateNumber = (input) => {
+  const numberRegex = /^[1-9]\d*$/;
+  if (!numberRegex.test(input)) {
+    return "Please enter a valid positive number.";
+  }
+  return true;
+};
+
+const validateOfficeNumber = (input) => {
+  const officeNumberRegex = /^[1-9]\d*$/;
+  if (!officeNumberRegex.test(input)) {
+    return "Please enter a valid office number (positive integer).";
+  }
+  return true;
+};
+
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email) || "Please enter a valid email.";
+};
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's the manager's name: ",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Manager's ID: ",
+    validate: validateNumber,
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What's the manager's email: ",
+    validate: validateEmail,
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Manager's office number: ",
+    validate: validateOfficeNumber,
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's the engineer's name: ",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Engineer's ID: ",
+    validate: validateNumber,
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email: ",
+    validate: validateEmail,
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Engineer's GitHub username: ",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's the intern's name: ",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Intern's ID: ",
+    validate: validateNumber,
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email: ",
+    validate: validateEmail,
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Intern's school: ",
+  },
+];
