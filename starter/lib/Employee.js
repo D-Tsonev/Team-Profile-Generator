7e72dc2f21a76ef6a51dc4d06d6160dbf05c66a3
Dimi Tsonev <dconev@gmail.com>
2024-01-22 18:36:11 +0000

    Employee class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,21 @@
+
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name
+    this.id = id
+    this.email = email 
+  }
+getName(name){
+  return this.name
+}
+getId(id){
+  return this.id
+}
+getEmail(email){
+  return this.email
+}
+
+}
+
+module.exports = Employee
